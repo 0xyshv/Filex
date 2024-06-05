cfd0d4faf155449f09f3fc8901ba4b825d37c8ee
0xyshv <writetoyshv@gmail.com>
2024-06-05 18:06:07 +0530

    chore: add file component ui

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-
 import Navbar from "@/components/navbar";
 import Upload from "@/components/upload";
 import { useRouter } from "next/navigation";
--- components/files.tsx
+++ components/files.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Files = () => {
-  return (
-    <div>Files</div>
-  )
-}
 
-export default Files
+  const [fileData, setfileData] = useState([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+
+  useEffect(() => {
+    console.log("Getting files...");
+    setLoading(false); //true
+  }, []);
+
+
+  return (
+    <div className="p-8 grid gap-8 ">
+      {loading ? (
+        <div className="flex justify-center mt-44">
+          <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center p-4">
+              Loading Files....
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          {fileData.length > 0 ? (
+            <>
+              {fileData.map((item, index) => (
+                <Card
+                  key={index}
+                  className="p-4 shadow-md "
+                >
+                  <CardHeader>
+                    <CardTitle className="flex justify-between">
+                      File Name
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent className="">
+                    File Name
+                  </CardContent>
+                  <CardFooter className="flex justify-between">
+                    File Name
+                  </CardFooter>
+                </Card>
+              ))}
+            </>
+          ) : (
+            <div className="flex justify-center p-40 font-bold text-xl text-gray-600">
+              No Files
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Files;

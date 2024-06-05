c1a3ea627c0f31e6735225c1f7fcaba1a1194a5d
0xyshv <writetoyshv@gmail.com>
2024-06-05 16:35:02 +0530

    feat: add providers

---
+++ components/providers.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import * as React from "react";
+import { WagmiProvider, createConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+const config = createConfig(
+  getDefaultConfig({
+    walletConnectProjectId:
+      process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+    chains: [sepolia],
+    appName: "BlockInvoice",
+  })
+);
+
+const queryClient = new QueryClient();
+
+export function Providers({ children, ...props }: ProvidersProps) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider theme="rounded">{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
